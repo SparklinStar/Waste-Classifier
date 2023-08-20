02a13bf42e50040d0b36051cc43a004afc1e9d4e
sparklinstar <souradip1000@gmail.com>
2023-08-20 17:02:43 +0530

    changed app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from PIL import Image
 import tensorflow as tf
 from tensorflow.keras.applications.efficientnet import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
 # Appname
 st.set_page_config(page_title="Everyday Object Classifier", layout="wide")
@@ -13,47 +14,37 @@ st.markdown("<h1 style='text-align: center; color: #000;'>Everyday Object Classi
 
 # Load your model and its weights
 model = tf.keras.models.load_model('hackomedfinaaaal.h5')
-class_names = ['Medical Hazardous Waste','Inorganic Waste','Organic Waste']  # List of your class names
+class_names = ['Medical Hazardous Waste', 'Inorganic Waste', 'Organic Waste']  # List of your class names
+
+# Create a VideoTransformer class
+class WebcamTransformer(VideoTransformerBase):
+    def __init__(self):
+        self.camera = cv2.VideoCapture(0)
+
+    def transform(self, frame):
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        resized_image = cv2.resize(image, (224, 224))  # Resize to your model's input size
+        image_array = img_to_array(resized_image)
+        image_array = np.expand_dims(image_array, axis=0)  # Add batch dimension
+        image_array = preprocess_input(image_array)  # Preprocess using the same function as during training
+
+        # Make a prediction
+        predictions = model.predict(image_array)
+        predicted_class = np.argmax(predictions[0])  # Assuming your model outputs class probabilities
+
+        # Print the prediction
+        predicted_probability = predictions[0][predicted_class]
+
+        return np.array(frame), f"Predicted class: {class_names[predicted_class]}, Probability: {predicted_probability*100:.2f}"
 
 # Define the Streamlit app
 def main():
     st.write("Upload an image for classification")
     st.title("Webcam Live Feed")
     run = st.checkbox('Run')
-    FRAME_WINDOW = st.image([])
 
-    if run: 
-        camera = cv2.VideoCapture(0)
-
-        while run:
-            _, frame = camera.read()
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            FRAME_WINDOW.image(image)
-
-            resized_image = cv2.resize(image, (224, 224))  # Resize to your model's input size
-            image_array = img_to_array(resized_image)
-            image_array = np.expand_dims(image_array, axis=0)  # Add batch dimension
-            image_array = preprocess_input(image_array)  # Preprocess using the same function as during training
-
-            # Make a prediction
-            predictions = model.predict(image_array)
-            predicted_class = np.argmax(predictions[0])  # Assuming your model outputs class probabilities
-
-            # Print the prediction
-            predicted_probability = predictions[0][predicted_class]
-            if np.isnan(predicted_probability).any():
-                st.write("No Waste Detected(Default)")
-            else:
-                st.write(f"Predicted class: {class_names[predicted_class]}")
-                st.write(f"Probability: {predicted_probability*100:.2f}")
-              # Probability of the predicted class
-
-
-            # Print the probability
-           
-        
-        st.write('Stopped')
-        camera.release()  # Release the camera when the loop is stopped
+    if run:
+        webrtc_streamer(key="example", video_transformer_factory=WebcamTransformer)
 
     items = [
         'Organic Waste', 'Inorganic Waste', 'Medical Hazardous Waste'
